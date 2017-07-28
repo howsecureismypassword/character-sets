02a6bbc9aab2280d6ea2ecbd3175286dbb37804f
Mark Wales <mark@smallhadroncollider.com>
2017-07-28 11:57:58 +0100

    added character set catch all

--- character-sets.js
+++ character-sets.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var unspecifiedDefault = 1e3;
+
 var L = require("hsimp-library");
 
 var characterSetsDictionary = {};
@@ -17,11 +19,19 @@ var characterSet = function (string) {
     }
 
     L.forEach(function (set, name) {
-        if (string.match(getRegex(set))) {
+        var length = string.length;
+        string = string.replace(RegExp(getRegex(set), "g"), "");
+
+        if (length !== string.length) {
             containedInSets.push(name);
             possibleCharacters += getValue(set);
         }
     }, characterSetsDictionary);
+
+    if (string.length) {
+        containedInSets.push("Unspecified Unicode");
+        possibleCharacters += unspecifiedDefault;
+    }
 
     self.getSets = function () {
         return containedInSets;
